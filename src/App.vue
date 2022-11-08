2080ad972549b33482b682efcda07a6484272ede
ruby <ruby@kuma-gaming.com>
2022-11-08 10:24:18 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,42 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+// import TheWelcome from './components/TheWelcome.vue'
+
+import { ref } from "vue";
+
+const headerLinks = ref([
+  { title: "關卡資訊", href: "https://2022.thef2e.com/news" },
+  { title: "作品列表", href: "https://2022.thef2e.com/works" },
+  { title: "攻略資源", href: "https://hexschool.tw/OsAcp" },
+  { title: "求職專區", href: "https://2022.thef2e.com/jobs" },
+]);
 </script>
 
 <template>
+  <header class="header px-12.5 py-5 flex justify-between">
+    <h1 class="bg-logo bg-cover bg-no-repeat bg-center">logo</h1>
+    <nav class="mr-9">
+      <a
+        v-for="(headerLink, idx) in headerLinks"
+        :key="headerLink.title"
+        :href="headerLink.href"
+        :class="{
+          'pr-2 mr-2 border-r text-secondary-100 border-secondary-100 text-lg':
+            headerLinks.length !== idx + 1,
+        }"
+      >
+        {{ headerLink.title }}</a
+      >
+    </nav>
+  </header>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="./assets/logo.svg"
+      width="125"
+      height="125"
+    />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,43 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          100: "#96F0FE",
+          200: "#ABF3FE",
+          300: "#B6F5FF",
+          400: "#96F0FE80",
+        },
+        warning: {
+          100: "#FFDC62",
+          200: "#FFE381",
+          300: "#FFE791",
+          400: "#FFEDB0",
+        },
+        secondary: {
+          100: "#252525",
+          200: "#656565",
+          300: "#C8C8C8",
+          400: "#EEEBEB",
+        },
+        info: {
+          100: "#2D3F58",
+          200: "#54758B",
+        },
+        danger: {
+          100: "#FEA996",
+        },
+      },
+      backgroundImage: {
+        logo: "url('/assets/images/Logo/logo.svg')",
+        "m-logo": "url('/assets/images/m-logo.svg')",
+      },
+      spacing: {
+        12.5: "52px",
+      },
+    },
   },
   plugins: [],
-}
+};
